3eacea2e90632026b52a766a34b66816a8960bdb
David Amsellem Quiros <64606076+DavidAmsellem@users.noreply.github.com>
2021-03-29 19:58:15 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,37 @@
 
 
+
+
+
+
+const LADlogon = document.querySelector('.LADlogon');
+
+window.onscroll = function(){
+
+    var top1 = window.scrollY;
+    console.log(top1);
+    if (top1 >= 30){
+        LADlogon.classList.add('active');
+        
+    }else{
+        LADlogon.classList.remove('active');
+        
+    }
+}
+
 const headerlad = document.querySelector('.headerlad');
 
 window.onscroll = function(){
 
     var top = window.scrollY;
     console.log(top);
-    if (top >= 100){
+    if (top >= 20){
         headerlad.classList.add('active');
+        LADlogon.classList.add('active');
         
     }else{
         headerlad.classList.remove('active');
+        LADlogon.classList.remove('active');
         
     }
 }
-
-
-
