dc30f1bbd02e444a79a0e8d5505974dae83d946f
David Amsellem Quiros <64606076+DavidAmsellem@users.noreply.github.com>
2021-03-29 19:06:42 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+const headerlad = document.querySelector('.headerlad');
+
+window.onscroll = function(){
+
+    var top = window.scrollY;
+    console.log(top);
+    if (top >= 100){
+        headerlad.classList.add('active');
+    }else{
+        headerlad.classList.remove('active');
+    }
+}
